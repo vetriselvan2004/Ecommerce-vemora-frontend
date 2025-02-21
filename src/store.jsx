ab9dff2afc99eccf3ee816f86ccd770fa64b67e6
vetriselvan2004 <selvanvetri448@gmail.com>
2025-02-21 12:32:04 +0530

    Fixed classnames import issue

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,6 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import {thunk} from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import {
   productListReducer,
   productDetailsReducer,
